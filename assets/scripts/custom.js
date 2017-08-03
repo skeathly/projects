d0cd3c8368b9fd738b8f2a895819a0b16c134646
Stan Keathly <skeathly@gmail.com>
2017-08-03 10:10:00 -0500

    GA ID Update

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -11,7 +11,7 @@ particlesJS.load('particles-js', 'particles.json', function() {
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-101864537-1', 'auto');
+ga('create', 'UA-37050325-1', 'auto');
 ga('send', 'pageview');
 
 /* Scroll Line */
